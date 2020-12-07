aafe194d5824fae0de1235a99ebddcdbc98d5425
freya022 <41875020+freya022@users.noreply.github.com>
2020-12-07 10:46:27 +0100

    Improve input readers

--- src/main/java/com/freya02/aoc/Utils.java
+++ src/main/java/com/freya02/aoc/Utils.java
@@ -1,35 +1,48 @@
 package com.freya02.aoc;
 
-import java.io.BufferedReader;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Utils {
-	private static Stream<String> getInputLinesStream(Class<?> callerClass) {
-		return new BufferedReader(new InputStreamReader(callerClass.getResourceAsStream(callerClass.getSimpleName() + ".txt"))).lines();
-	}
-
 	public static List<String> getInputLines() {
 		final Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
 
-		return getInputLinesStream(callerClass).collect(Collectors.toList());
+		try {
+			return Files.readAllLines(getInputPath(callerClass));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static Stream<String> getInputLinesStream() {
 		final Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
 
-		return getInputLinesStream(callerClass);
+		try {
+			return Files.lines(getInputPath(callerClass));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static String getInput() {
 		final Class<?> callerClass = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).getCallerClass();
 
 		try {
-			return Files.readString(com.freya02.misc.Utils.getJarFilesystem(callerClass).resolve(callerClass.getPackageName().replace(".", "/")).resolve(callerClass.getSimpleName() + ".txt"));
+			return Files.readString(getInputPath(callerClass));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@NotNull
+	private static Path getInputPath(Class<?> callerClass) {
+		try {
+			return com.freya02.misc.Utils.getJarFilesystem(callerClass).resolve(callerClass.getPackageName().replace(".", "/")).resolve(callerClass.getSimpleName() + ".txt");
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
